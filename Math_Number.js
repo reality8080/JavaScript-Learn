b1f39dae0f15d0f490ce8b49ee3755afc8da15d2
Nguyen Tran Thien Phu <12a1nguyentranthienphu2023@gmail.com>
2025-01-29 11:26:17 +0700

    add MMath_MathNumbers

---
+++ Math_Number.js
@@ -0,0 +1,27 @@
+// Có thể dùng cộng trừ nhân chia, theo thứ tự ưu
+// tiên, 
+//Làm tròn số thập phân
+// Syntax: Math.floor(number)
+// Example: Math.floor(3.14)
+// Làm tròn xuống
+Math.floor(3.14); // 3
+Math.floor(3.99); // 3
+Math.floor(3.5); // 3
+Math.floor(3); // 3
+// Làm tròn lên
+Math.ceil(3.14); // 4
+Math.ceil(3.99); // 4
+Math.ceil(3.5); // 4
+Math.ceil(3); // 3
+// Lam tròn một nữa, phần thập phân thức nhất là
+// Lớn hơn hoặc bằng 5 thì sẽ làm tròn lên, ngược
+//  lại làm tròn xuống
+Math.round(3.14); // 3
+Math.round(3.99); // 4
+Math.round(3.5); // 4
+Math.round(3); // 3
+
+// Random một số thực trong khoảng [0,1), nghĩa là
+//  không lấy 1
+
+Math.random(); // [0, 1)
